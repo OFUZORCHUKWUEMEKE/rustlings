1d39a7b9785c066464f4481412c32382c5aae981
OFUZORCHUKWUEMEKE <uniben2018@gmail.com>
2025-07-13 23:46:34 +0100

    update

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,6 +4,8 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        let number = element * 2;
+        output.push(number);
     }
 
     output
@@ -24,6 +26,8 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     input
         .iter()
         .map(|element| {
+           let num= *element * 2;
+           num
             // ???
         })
         .collect()
